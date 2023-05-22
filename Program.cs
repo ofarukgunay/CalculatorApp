29948c2d188b22acf887aaebb167b269396cd43c
Ömer Faruk Günay <116501544+ofarukgunay@users.noreply.github.com>
2023-05-22 17:36:04 +0300

    C# folder added

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        double sayi1, sayi2, sonuc;
+        string islem;
+
+        Console.WriteLine("İlk sayıyı girin: ");
+        sayi1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("İkinci sayıyı girin: ");
+        sayi2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Yapılacak işlemi girin (+, -, *, /): ");
+        islem = Console.ReadLine();
+
+        switch (islem)
+        {
+            case "+":
+                sonuc = sayi1 + sayi2;
+                Console.WriteLine(sayi1 + " + " + sayi2 + " = " + sonuc);
+                break;
+
+            case "-":
+                sonuc = sayi1 - sayi2;
+                Console.WriteLine(sayi1 + " - " + sayi2 + " = " + sonuc);
+                break;
+
+            case "*":
+                sonuc = sayi1 * sayi2;
+                Console.WriteLine(sayi1 + " * " + sayi2 + " = " + sonuc);
+                break;
+
+            case "/":
+                if (sayi2 == 0)
+                {
+                    Console.WriteLine("Hata: Sıfıra bölme hatası!");
+                }
+                else
+                {
+                    sonuc = sayi1 / sayi2;
+                    Console.WriteLine(sayi1 + " / " + sayi2 + " = " + sonuc);
+                }
+                break;
+
+            default:
+                Console.WriteLine("Hatalı işlem seçimi!");
+                break;
+        }
+
+        Console.ReadLine();
+    }
+
+
+}
